6f04a95e57a529e4f6fa0f697ea20dfcd2e799b6
Jeff LANCE <email@jefflance.me>
2024-03-09 13:20:28 +0100

    Up.

--- tmsu.py
+++ tmsu.py
@@ -1,23 +1,6 @@
-#!/usr/bin/python3
-#
-# Copyright 2017 Hasan Yavuz Ozderya
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 import sys, os, enum
 import subprocess as sp
+
 
 class Tmsu:
     def __init__(self, tmsu):
@@ -25,17 +8,18 @@ class Tmsu:
 
     def info(self):
         try:
-            r = self._cmd('info')
+            r = self._cmd("info")
         except sp.CalledProcessError as e:
-            if e.returncode == 1: # database doesn't exist
+            if e.returncode == 1:  # database doesn't exist
                 return None
         lines = r.splitlines()
-        def psplit(l): return map(lambda x: x.strip(), l.split(':'))
+
+        def psplit(l):
+            return map(lambda x: x.strip(), l.split(":"))
+
         d = dict(map(psplit, lines))
 
-        return {'root': d['Root path'],
-                'size': d['Size'],
-                'database':d['Database']}
+        return {"root": d["Root path"], "size": d["Size"], "database": d["Database"]}
 
     def tags(self, fileName=None):
         """Returns a list of tags. If fileName is provided, list item is a tuple of
@@ -43,11 +27,11 @@ class Tmsu:
         if fileName:
             # Note: tmsu behaves differently for 'tags' command when used
             # interactively and called from scripts. That's why we add '-n'.
-            #r = self._cmd('tags -n "{}"'.format(fileName))
+            # r = self._cmd('tags -n "{}"'.format(fileName))
             r = self._cmd('tags -1 -n never "{}"'.format(fileName))
             tag_value = []
             tag_value = r.splitlines()
-            #for tag in r.splitlines():
+            # for tag in r.splitlines():
             #    tv = tag.split("=")
             #    if len(tv) > 1:
             #        tag_value.append((tv[0], tv[1]))
@@ -55,12 +39,13 @@ class Tmsu:
             #        tag_value.append((tv[0], ""))
             return tag_value
         else:
-            return self._cmd('tags').splitlines()
+            return self._cmd("tags").splitlines()
 
     def tag(self, fileName, tagName, value=None):
         try:
-            self._cmd('tag "{}" {}{}'.format(fileName, tagName,
-                                             "="+value if value else ""))
+            self._cmd(
+                'tag "{}" {}{}'.format(fileName, tagName, "=" + value if value else "")
+            )
             return True
         except sp.CalledProcessError as e:
             print("Failed to tag file.")
@@ -68,8 +53,11 @@ class Tmsu:
 
     def untag(self, fileName, tagName, value=None):
         try:
-            self._cmd('untag "{}" {}{}'.format(fileName, tagName,
-                                               "="+value if value else ""))
+            self._cmd(
+                'untag "{}" {}{}'.format(
+                    fileName, tagName, "=" + value if value else ""
+                )
+            )
             return True
         except sp.CalledProcessError as e:
             print("Failed to untag file.")
@@ -77,7 +65,7 @@ class Tmsu:
 
     def rename(self, tagName, newName):
         try:
-            self._cmd('rename {} {}'.format(tagName, newName))
+            self._cmd("rename {} {}".format(tagName, newName))
             return True
         except sp.CalledProcessError as e:
             print("Failed to rename tag.")
@@ -85,7 +73,7 @@ class Tmsu:
 
     def values(self, tagName=None):
         try:
-            r = self._cmd('values {}'.format(tagName if tagName else ""))
+            r = self._cmd("values {}".format(tagName if tagName else ""))
             return r.splitlines()
         except sp.CalledProcessError as e:
             print("Failed to get value list.")
@@ -93,19 +81,22 @@ class Tmsu:
 
     def delete(self, tagName):
         try:
-            self._cmd('delete {}'.format(tagName))
+            self._cmd("delete {}".format(tagName))
             return True
         except sp.CalledProcessError as e:
             print("Failed to delete tag: {}".format(tagName))
             return False
 
     def _cmd(self, cmd):
-        return sp.check_output('tmsu ' + cmd, shell=True, stderr=sp.DEVNULL).decode('utf-8')
+        return sp.check_output("tmsu " + cmd, shell=True, stderr=sp.DEVNULL).decode(
+            "utf-8"
+        )
 
     @staticmethod
     def findTmsu():
         import shutil
-        tmsu =  shutil.which("tmsu")
+
+        tmsu = shutil.which("tmsu")
         if tmsu:
             return Tmsu(tmsu)
         else:
